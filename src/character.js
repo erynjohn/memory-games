5e2108516794587d5d0ac7456272b1c9eeaf55ed
Eryn <johnmary1010@gmail.com>
2019-04-29 19:54:21 -0500

    update

--- src/character.js
+++ src/character.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import Nav from './nav';
 import Image from 'react-bootstrap/Image'
 import { Container, Row, Col } from 'react-bootstrap';
+import './shake.css';
+
 
 class Character extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isClicked: false
+            isClicked: false,
+            addClass: false
         }
         this.handleClick = this.handleClick.bind(this);
     }
     // check if image is clicked
     handleClick() {
         this.setState(prevState => ({
-            isClicked: !prevState.isClicked
+            isClicked: !prevState.isClicked 
+
         }))
         console.log(this)
     }
-    
     render() {
         return (
             <div>
@@ -28,49 +31,49 @@ class Character extends Component {
                     <Container>
                         <Row>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                                <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img1.jpg'} roundedCircle />
                             </Col>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img2.jpg'} roundedCircle />
                             </Col>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img3.jpg'} roundedCircle />
                             </Col>
 
                         </Row>
                         <Row>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img4.jpg'} roundedCircle />
                             </Col>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img5.jpg'} roundedCircle />
                             </Col>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
-                            </Col>
-                        </Row>
-
-                        <Row>
-                            <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img6.jpg'} roundedCircle />
                             </Col>
                         </Row>
 
                         <Row>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img7.jpg'} roundedCircle />
                             </Col>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img8.jpg'} roundedCircle />
                             </Col>
                             <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} style={{ width: 171, height: 180 }} src="http://annawrites.com/blog/wp-content/uploads/2012/02/family-guy.jpg" roundedCircle />
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img9.jpg'} roundedCircle />
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col xs={3} md={4}>
+                                <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img10.jpg'} roundedCircle />
+                            </Col>
+                            <Col xs={3} md={4}>
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img11.jpg'} roundedCircle />
+                            </Col>
+                            <Col xs={3} md={4}>
+                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img12.jpg'} roundedCircle />
                             </Col>
                         </Row>
 
