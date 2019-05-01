d67b29a6309d2725b4d13c9cf0876872d1054724
Eryn <johnmary1010@gmail.com>
2019-05-01 17:15:08 -0500

    updated character map

--- src/character.js
+++ src/character.js
@@ -1,87 +1,50 @@
 import React, { Component } from 'react';
 import Nav from './nav';
-import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image';
+import Characters from './characters.json';
 import { Container, Row, Col } from 'react-bootstrap';
-import './shake.css';
 
 
 class Character extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isClicked: false,
-            addClass: false
+            clickCount: 0
         }
-        this.handleClick = this.handleClick.bind(this);
-    }
-    // check if image is clicked
-    handleClick() {
-        this.setState(prevState => ({
-            isClicked: !prevState.isClicked 
 
-        }))
-        console.log(this)
+    }
+
+    // check if image is clicked
+    handleClick = (id) => {
+
+        this.setState((prevState, props) => ({
+            clickCount: prevState.clickCount
+        }), () => {
+            console.log(this.state.clickCount)
+            if (this.state.clickCount >= 2) {
+                console.log("try again")
+            }
+
+        }
+        )
     }
     render() {
         return (
-            <div>
-            <style>{'body { background-color: rgba(0,0,0, 0.1);}'}</style>
+            <>
                 <Nav />
-                <>
-                    <Container>
+                <Container>
                         <Row>
-                            <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img1.jpg'} roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img2.jpg'} roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img3.jpg'} roundedCircle />
-                            </Col>
-
-                        </Row>
-                        <Row>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img4.jpg'} roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img5.jpg'} roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img6.jpg'} roundedCircle />
+                            <Col xs={4} md={11}>
+                {Characters.map((character) => {
+                    return <Image width={250}  src={character.image} key={character.id} rounded />
+                            })}
                             </Col>
                         </Row>
-
-                        <Row>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img7.jpg'} roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img8.jpg'} roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img9.jpg'} roundedCircle />
-                            </Col>
-                        </Row>
-
-                        <Row>
-                            <Col xs={3} md={4}>
-                                <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img10.jpg'} roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img11.jpg'} roundedCircle />
-                            </Col>
-                            <Col xs={3} md={4}>
-                            <Image onClick={this.handleClick} className='img' src={process.env.PUBLIC_URL + '/images/img12.jpg'} roundedCircle />
-                            </Col>
-                        </Row>
-
-
                     </Container>
-                </>
-            </div>
+
+            </>
         )
     }
 }
 export default Character;
+
