68b1347017a2567414de7451d5539839c4c5b71a
Eryn <johnmary1010@gmail.com>
2019-05-02 15:44:25 -0500

    added score function

--- src/character.js
+++ src/character.js
@@ -3,27 +3,38 @@ import Nav from './nav';
 import Image from 'react-bootstrap/Image';
 import Characters from './characters.json';
 import { Container, Row, Col } from 'react-bootstrap';
-import Shuffle from 'react-shuffle';
 
 
 class Character extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isclicked: false
-
+            clicked: [],
+            score: 0
         }
 
     }
 
-    handleClick = ((event) => {
+    handleClick = ((event, key) => {
         let click = event.target.id
         console.log(click)
+        this.setState({clicked:click}, ()=>{
+            if(click) {
+                this.setState({score: this.state.score +1})
+
+                Characters.sort(() => {
+                    return 0.5 - Math.random()
+                })
+            }
+        })
     })
     render() {
         return (
             <>
                 <Nav />
+                <div style={{backgroundColor: 'rgb(219,217,209, 0.5)'}}>
+                    <h2>Score: {this.state.score}</h2>
+                </div>
                 <Container>
                         <Row>
                             <Col xs={4} md={11}>
